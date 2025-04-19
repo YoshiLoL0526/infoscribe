f8c2bdbe632da9e0dee2153de7e4c0113b2ff37e
Raidel <yoshilol0526@gmail.com>
2025-04-19 20:05:13 -0300

    Fix dockerization

--- backend/app/services/headlines_service.py
+++ backend/app/services/headlines_service.py
@@ -1,5 +1,6 @@
+from app.core.config import settings
 from scrape_hn import HackerNewsIntegration
 
 
 def get_headlines_service() -> HackerNewsIntegration:
-    return HackerNewsIntegration()
+    return HackerNewsIntegration(driver_url=settings.REMOTE_DRIVER_URL)
